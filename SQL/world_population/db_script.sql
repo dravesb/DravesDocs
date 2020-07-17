0a7ea9a8e974ed5653be09d04ea893c87d942bf3
Benjamin Draves <benjamin.draves@gmail.com>
2020-07-17 14:07:29 -0400

    adding basic SQL practice

--- SQL/world_population/db_script.sql
+++
@@ -1,3 +0,0 @@
- SELECT * 
- FROM population_years
- LIMIT 10; 
--- SQL/world_population/population_queries.sql
+++ SQL/world_population/population_queries.sql
@@ -1,6 +1,32 @@
 -- This is the first query:
 
-SELECT DISTINCT year from population_years;
+SELECT DISTINCT year 
+FROM population_years
+LIMIT 10;
 
 -- Add your additional queries below:
 
+SELECT population 
+FROM population_years
+WHERE country = 'Gabon' 
+ORDER BY population DESC
+LIMIT 1;
+
+SELECT country, population 
+FROM population_years
+WHERE year = '2005'
+ORDER BY population 
+LIMIT 10;   
+
+SELECT DISTINCT country 
+FROM population_years
+WHERE year = '2010' AND population > 100;
+
+SELECT DISTINCT country
+FROM population_years
+WHERE country LIKE '% Islands %'; 
+
+SELECT * 
+FROM population_years
+WHERE country = 'Indonesia' AND year = '2000' 
+OR country = 'Indonesia' AND year = '2010'; 
